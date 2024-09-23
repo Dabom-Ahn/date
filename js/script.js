84c7f88527d69570b2c88cfe26e38983bd3b0b06
Dabom Ahn <900428star@naver.com>
2024-09-23 11:52:19 +0900

    클래스명에 따른 화면 테마 변경

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
-const numbers = document.querySelectorAll('.screen span');
-const[am, pm] = document.querySelectorAll('.screen em')
+const main = document.querySelector("main");
+const numbers = document.querySelectorAll(".screen span");
+const [am, pm] = document.querySelectorAll(".screen em");
 
-setInterval(()=>{
-    getTime().forEach((num, idx)=> setTime(num, idx));
+setInterval(() => {
+  getTime().forEach((num, idx) => setTime(num, idx));
 }, 1000);
 
 //시간값을 구해서 반환하는 함수
-function getTime(){
-    const now = new Date();
-    let hr = now.getHours();
-    let min = now.getMinutes();
-    let sec = now.getSeconds();
+function getTime() {
+  const now = new Date();
+  let hr = now.getHours();
+  let min = now.getMinutes();
+  let sec = now.getSeconds();
 
-    if(hr>=13) {
-        am.classList.remove('on');
-        pm.classList.add('on');
-    } else {
-        pm.classList.remove('on');
-        am.classList.add('on');
-    }
-    return [hr,min, sec];
+  if (hr >= 13) {
+    am.classList.remove("on");
+    pm.classList.add("on");
+  } else {
+    pm.classList.remove("on");
+    am.classList.add("on");
+  }
+  return [hr, min, sec];
 }
 
 //반환된 시간값을 DOM에 세팅하는 함수
-function setTime(num, index){
-    num = num <10 ? '0' + num : num;
-    numbers[index].innerText = num;
+function setTime(num, index) {
+  num = num < 10 ? "0" + num : num;
+  numbers[index].innerText = num;
 }
 
+//시간에 따른 테마 변경 함수
+function changeTheme() {
+  const hr = new Date().getHours();
+
+  if (hr >= 5 && hr < 11) {
+  }
+}
